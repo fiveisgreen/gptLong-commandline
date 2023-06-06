1a068ada65e48835f4398b7de4e3cf5c5b5d9c38
fiveisgreen <4203265+fiveisgreen@users.noreply.github.com>
2023-06-06 15:58:31 +0200

    fixed syntax error in bash if's

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ if [ "$SHELL" = "/bin/sh" ]; then
     RCfile="$HOME/.profile"
 fi
 
-if [ grep -q "OPEN_AI_KEY" $RCfile]; then
+if [ `grep -q "OPEN_AI_KEY" $RCfile` ]; then
   echo "OpenAI API key detected. If you need to change this, edit the OPENAI_API_KEY in $RCfile."
 else
   echo "Enter your OpenAI license key below. You can get your key by going to https://platform.openai.com/account/api-keys and hitting ''Create new secret key''"
@@ -42,21 +42,21 @@ else
   echo "export OPENAI_API_KEY=$LICENSE_KEY" >> $RCfile
 fi
 
-if [ ! grep -q "alias gpt=" $RCfile ]; then
+if [ ! `grep -q "alias gpt=" $RCfile` ]; then
   echo "alias gpt='python3 `pwd`/api/gpt_command_prompt.py'" >> $RCfile
   echo "Command 'gpt' is now defined."
 fi
 
-if [ ! grep -q "alias gtp=" $RCfile ]; then
+if [ ! `grep -q "alias gtp=" $RCfile` ]; then
   echo "alias gtp='python3 `pwd`/api/gpt_command_prompt.py'" >> $RCfile
 fi
 
-if [ ! grep -q "alias gpte=" $RCfile ]; then
+if [ ! `grep -q "alias gpte=" $RCfile` ]; then
   echo "alias gpte='python3 `pwd`/api/gpt_command_prompt_edit_loop.py'" >> $RCfile
   echo "Command 'gpte' is now defined."
 fi
 
-if [ ! grep -q "alias gtpe=" $RCfile ]; then
+if [ ! `grep -q "alias gtpe=" $RCfile` ]; then
   echo "alias gtpe='python3 `pwd`/api/gpt_command_prompt_edit_loop.py'" >> $RCfile
 fi
 source $RCfile
