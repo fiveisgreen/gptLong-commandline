ab8583204c2b61efa04551d0b8b80373dfdde93a
fiveisgreen <4203265+fiveisgreen@users.noreply.github.com>
2023-06-09 11:09:15 +0200

    imporved gpt_simplest_API_test.py

--- tests/gpt_simplest_API_test.py
+++ tests/gpt_simplest_API_test.py
@@ -9,12 +9,16 @@ Model = "text-davinci-003" #GPT-3
 #Model = "code-davinci-002"
 #Model = "text-davinci-002" "GPT-2
 
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = OPENAI_API_KEY 
+print("Loaded OPENAI_API_KEY", OPENAI_API_KEY)
+
 Prompt = "What is the capital of Africa?"
 print("Prompt", Prompt)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 Response = openai.Completion.create( model=Model, prompt=Prompt)
 
 print("Response:", Response.choices[0].text) 
-#text-davinci-003 usally says there is no one capital of Africa
+#Correct behavior: A responce from the AI saying there is no one capital of Africa
+#Incorrect output: Crashing, probalby with an openAI.error.AuthenticationError
 
