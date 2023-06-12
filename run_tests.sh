71950bbb9c7174c8cc98125a9c70c68a4a4d4bda
fiveisgreen <4203265+fiveisgreen@users.noreply.github.com>
2023-06-12 14:10:43 +0200

    changed testing

--- run_tests.sh
+++ run_tests.sh
@@ -7,5 +7,5 @@ gpt "What is the capital of France?" --verbose
 cat ./tests/test_gpte1_readme.txt
 gpte -f tests/umerr.txt -o tests/out.txt -p 1 "Remove 'um's and other non-word blather from this transcript"
 
-cat ./tests/test_gpte2_readme.txt
-gpte -f tests/htmltable.txt -i tests/inst.txt
+#cat ./tests/test_gpte2_readme.txt
+#gpte -f tests/htmltable.txt -i tests/inst.txt
---
+++ run_tests2.sh
@@ -0,0 +1,2 @@
+cat ./tests/test_gpte2_readme.txt
+gpte -f tests/htmltable.txt -i tests/inst.txt
