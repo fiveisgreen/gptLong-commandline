5cd78418b0366a6ac817300beae2ca735010da58
fiveisgreen <4203265+fiveisgreen@users.noreply.github.com>
2023-10-11 13:21:22 +0200

    gpt4 works for me now!

--- src/chatGPT.py
+++ src/chatGPT.py
@@ -14,14 +14,13 @@ if len(sys.argv) >= 2:
     System_Prompt = sys.argv[1]
 
 #Model = "gpt-3.5-turbo" #ChatGPT
-Model = "gpt-4-0613" #If this works for you, call me. We'll throw a party.
-#Model = "gpt-4" #If this works for you, call me. We'll throw a party.
+Model = "gpt-4"
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 print("Loaded OPENAI_API_KEY", OPENAI_API_KEY[:6]+"..."+OPENAI_API_KEY[-6:])
 openai.api_key = OPENAI_API_KEY 
 
-message = {"role":"user", "content": input("This is the beginning of your chat with AI. [To exit, send \"###\".]\n\nYou: ")};
+message = {"role":"user", "content": input(f"This is the beginning of your chat with {Model}. [To exit, send \"###\".]\n\nYou: ")};
 
 conversation = [{"role": "system", "content": System_Prompt}]
 
